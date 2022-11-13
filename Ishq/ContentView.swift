d52ed505e627a676dfe1e61186e21f730c1c52c9
Shashank Kothapalli <shakotha@gmail.com>
2022-11-13 16:38:09 -0500

    Initial Commit

---
+++ Ishq/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ishq
+//
+//  Created by Shashank Kothapalli on 11/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ishq/IshqApp.swift
@@ -0,0 +1,17 @@
+//
+//  IshqApp.swift
+//  Ishq
+//
+//  Created by Shashank Kothapalli on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct IshqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
