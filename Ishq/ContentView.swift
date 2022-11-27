d466ec5b9f7507aba260c003bca803bde343efb2
Sishir Mohan <sishirmohan@Sishirs-MBP.lan>
2022-11-27 18:40:22 -0500

    first commit of firebase phone auth and realm auth

--- Ishq/ContentView.swift
+++ Ishq/ContentView.swift
@@ -2,18 +2,27 @@
 //  ContentView.swift
 //  Ishq
 //
-//  Created by Shashank Kothapalli on 11/13/22.
+//  Created by Sishir Mohan on 11/22/22.
 //
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 struct ContentView: View {
+    @AppStorage("auth_status") var auth_status = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            if auth_status{
+                Text("Home").navigationTitle("Home")
+            } else {
+                PhoneAuthentication()
+            }
+        }
+        
     }
 }
 
+@available(iOS 15.0, *)
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
